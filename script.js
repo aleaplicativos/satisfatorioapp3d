068660cbee0c45de57ee1aef38424d2a9802e282
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-06-18 12:19:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const games = [
+    {      
+      link: "https://aleaplicativos.github.io/aranhacolorida/"
+    },
+    {      
+      link: "https://aleaplicativos.github.io/dripdripdrip/"
+    },
+    
+    {
+      
+      link: "https://aleaplicativos.github.io/lapis3d/"
+    },
+        
+    // Adicione quantos jogos desejar nesta lista
+  ];
+  
+  const gameContainer = document.getElementById("game-container");
+  const gameWrapper = document.getElementById("game-wrapper");
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  
+  let currentGameIndex = -1;
+  
+  function loadGame() {
+    const randomIndex = Math.floor(Math.random() * games.length);
+    currentGameIndex = randomIndex;
+    gameWrapper.innerHTML = `<iframe src="${games[currentGameIndex].link}" width="100%" height="100%"></iframe>`;
+  }
+  
+  function navigateToPreviousGame() {
+    if (currentGameIndex === 0) {
+      return;
+    }
+  
+    currentGameIndex--;
+    loadGame();
+  }
+  
+  function navigateToNextGame() {
+    if (currentGameIndex === games.length - 1) {
+      return;
+    }
+  
+    currentGameIndex++;
+    loadGame();
+  }
+  
+  previousButton.addEventListener("click", navigateToPreviousGame);
+  nextButton.addEventListener("click", navigateToNextGame);
+  
+  loadGame();
+  
