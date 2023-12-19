372fc5fc75939dac68a823fe02abc1a11cde6bd1
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-12-19 11:34:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const games = [
     {link: "https://aleaplicativos.github.io/luminescence/"},
     {link: "https://aleaplicativos.github.io/piston-3d/"},
     {link: "https://aleaplicativos.github.io/olho-crazy/"},
+    {link: "https://aleaplicativos.github.io/Reach-out/"},
     {link: "https://aleaplicativos.github.io/3taint/"},
         
     // Adicione quantos jogos desejar nesta lista
